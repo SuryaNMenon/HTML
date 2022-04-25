1ba0fafedbc9e6a0806b6495c118f11c56f24cb1
Surya <suryanmenon@ieee.org>
2022-04-25 10:10:21 +0530

    Updated code

--- Lab Cycle 6/main.js
+++ Lab Cycle 6/main.js
@@ -2,14 +2,10 @@ $('#addTask').click(function(e){
     let task = $('#taskInput').val();
     $('ul').append(`<li>${task}<button class="X">X</button></li>`);
     $('#taskInput').val("");
-    $(".X").click(function(e)
-    {
+});
+
+$(".X").click(function(e){
         $(e.target.parentElement).detach();
-    });
-    $('li').click(function(){
-        $(this).css('background-color', 'lightgreen');
-        $(this).css('text-decoration', 'line-through');
-    })
 });
 
 $('li').click(function(){
